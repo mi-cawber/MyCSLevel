a4897c009f34e60d8e5d939f1b56b4eb38144ca0
mi-cawber <106614898+mi-cawber@users.noreply.github.com>
2024-10-27 14:00:48 -0400

    beating head against wall

---
+++ MyCSLevel.java
@@ -0,0 +1,12 @@
+public class MyCSLevel {
+    public static void main(String[] args) {
+        double multiplier = 1.0;
+        int level = 1;
+        double totalHours;
+        double hoursToNextLevel;
+        double hoursNeeded = hoursToNextLevel - totalHours;
+
+
+    }
+}
+
