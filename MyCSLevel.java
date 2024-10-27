91b8486b9473692de3a601f1ac960ad166913922
mi-cawber <106614898+mi-cawber@users.noreply.github.com>
2024-10-27 15:10:55 -0400

    getting somewhere

--- MyCSLevel.java
+++ MyCSLevel.java
@@ -1,10 +1,35 @@
+import java.util.Scanner;
+
 public class MyCSLevel {
     public static void main(String[] args) {
-        double multiplier = 1.0;
-        int level = 1;
-        double totalHours;
-        double hoursToNextLevel;
-        double hoursNeeded = hoursToNextLevel - totalHours;
+        //Scanner object
+        Scanner sc = new Scanner(System.in);
+
+        //basic variables
+        int level = 50;
+        int totalHours = 0, inputHours;
+        double hoursToNextLevel, hoursNeeded;
+        double multiplier = 5.0;
+        double hoursThisLevel = 0;
+
+
+        //the holy formula for leveling
+        hoursToNextLevel = multiplier * Math.log(level + 1);
+
+        //capture additional hours
+        System.out.print("input hours: ");
+        inputHours = sc.nextInt();
+        //add to hours this level
+        hoursThisLevel += inputHours;
+
+        if (hoursThisLevel >= hoursToNextLevel) {
+            level++;
+            hoursToNextLevel = 0;
+            System.out.printf("You have risen to %d", level);
+        } else {
+            System.out.printf("Hours needed %f", hoursToNextLevel - hoursThisLevel);
+        }
+
 
 
     }
